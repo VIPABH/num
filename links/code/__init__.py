bc945f34bd2c75b66e49a7fc46bca251e957dd33
vipabh2 <a8919832@googlemail.com>
2025-03-23 03:19:50 +0300

    Create __init__.py

---
+++ links/code/__init__.py
@@ -0,0 +1,7 @@
+import os 
+api_id = os.getenv('API_ID')      
+api_hash = os.getenv('API_HASH')  
+bot_token = os.getenv('BOT_TOKEN')
+ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+print("its worke")
+ABH.run_until_disconnected()
