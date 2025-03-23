13f1f030e1da0d94fb0e65d70e1669ea8eae45bc
vipabh2 <a8919832@googlemail.com>
2025-03-23 04:27:31 +0300

    Update __init__.py

--- links/code/__init__.py
+++ links/code/__init__.py
@@ -1,17 +1,17 @@
-import os
-from telethon import TelegramClient, events, Button
+# import os
+# from telethon import TelegramClient, events, Button
 
-try:
-    api_id = os.getenv('API_ID')
-    api_hash = os.getenv('API_HASH')
-    bot_token = os.getenv('BOT_TOKEN')
+# try:
+#     api_id = os.getenv('API_ID')
+#     api_hash = os.getenv('API_HASH')
+#     bot_token = os.getenv('BOT_TOKEN')
 
-    if not api_id or not api_hash or not bot_token:
-        raise ValueError("One or more required environment variables are missing.")
+#     if not api_id or not api_hash or not bot_token:
+#         raise ValueError("One or more required environment variables are missing.")
 
-    ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
-    print("Bot started successfully!")
-    ABH.run_until_disconnected()
+#     ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+#     print("Bot started successfully!")
+#     ABH.run_until_disconnected()
 
-except Exception as e:
-    print(f"An error occurred: {e}")
+# except Exception as e:
+#     print(f"An error occurred: {e}")
