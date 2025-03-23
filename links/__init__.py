d88bf15a2316a176cf91e0db78cdea0da94f7d22
vipabh2 <a8919832@googlemail.com>
2025-03-24 00:51:38 +0300

    Update __init__.py

--- links/__init__.py
+++ links/__init__.py
@@ -1,8 +1,8 @@
-from telethon import TelegramClient
-import os
-api_id = os.getenv('API_ID')      
-api_hash = os.getenv('API_HASH')  
-bot_token = os.getenv('BOT_TOKEN')
-ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
-print('its run')
-ABH.run_until_disconnected()
+# from telethon import TelegramClient
+# import os
+# api_id = os.getenv('API_ID')      
+# api_hash = os.getenv('API_HASH')  
+# bot_token = os.getenv('BOT_TOKEN')
+# ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+# print('its run')
+# ABH.run_until_disconnected()
