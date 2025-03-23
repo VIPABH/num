e3e541635b908a655e1d53886497e2e0859713f1
vipabh2 <a8919832@googlemail.com>
2025-03-23 03:40:01 +0300

    Update __main__.py

--- links/code/code/__main__.py
+++ links/code/code/__main__.py
@@ -1,7 +1,11 @@
+# links/code/__main__.py
 from .code import code
 from .code import code8
+
 def main():
     print("This is the main entry point of the 'links' package")
+    code()  # استدعاء دالة code
+    code8()  # استدعاء دالة code8
 
 if __name__ == '__main__':
     main()
