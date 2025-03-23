2203ac2a279d05a3a526eb4e42cac4e24fc6795b
vipabh2 <a8919832@googlemail.com>
2025-03-24 01:28:01 +0300

    Update __init__.py

--- links/code/__init__.py
+++ links/code/__init__.py
@@ -1,16 +1,1 @@
-import os
-from telethon import TelegramClient
-
-# تحميل المتغيرات
-api_id = os.getenv('API_ID')
-api_hash = os.getenv('API_HASH')
-bot_token = os.getenv('BOT_TOKEN')
-
-if not api_id or not api_hash or not bot_token:
-    raise ValueError("One or more required environment variables are missing.")
-
-# إنشاء كائن البوت ليكون متاحًا للاستيراد
-ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
-
-# استيراد الدوال لجعلها متاحة عند استيراد `code`
-from .module1 import reply
+from .abh import reply, reply_abh
