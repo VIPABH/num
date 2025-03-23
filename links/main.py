fdd51150946a95f8ea4b2a4a74690924d6ae5cd1
vipabh2 <a8919832@googlemail.com>
2025-03-24 00:52:03 +0300

    Update main.py

--- links/main.py
+++ links/main.py
@@ -2,11 +2,17 @@
 from code.module1 import some_function
 from code.module2 import function2
 from code.__init__ import ABH  # استيراد ABH من links/__init__.py
-
+from telethon import TelegramClient
+import os
+api_id = os.getenv('API_ID')      
+api_hash = os.getenv('API_HASH')  
+bot_token = os.getenv('BOT_TOKEN')
+ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+print('its run')
 def main():
     print("This is the main program!")
     some_function()
     function2()
-
 if __name__ == "__main__":
     main()
+ABH.run_until_disconnected()
