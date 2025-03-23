bf5ae482035ad2ae63e45d64d7e7b38e6a6d94aa
vipabh2 <a8919832@googlemail.com>
2025-03-23 03:49:31 +0300

    Update __main__.py

--- links/code/code/__main__.py
+++ links/code/code/__main__.py
@@ -1,14 +1,19 @@
-# links/code/__main__.py
+import asyncio
 from .code import code
 from .code import code8
 
+# دالة main غير المتزامنة
 async def main():
     print("This is the main entry point of the 'links' package")
-    code()  # استدعاء دالة code
-    code8()  # استدعاء دالة code8
-    await l313l.connect()
+    await code()  # استدعاء دالة code بشكل غير متزامن
+    await code8()  # استدعاء دالة code8 بشكل غير متزامن
+    await l313l.connect()  # استدعاء connect بشكل غير متزامن
+
+# دالة لبدء تحميل الإضافات بشكل غير متزامن
 async def startup_process():
     await load_plugins("code")
     await load_plugins("code8")
+
+# تشغيل دالة main باستخدام asyncio.run
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())  # استخدام asyncio.run لتشغيل الدالة main
