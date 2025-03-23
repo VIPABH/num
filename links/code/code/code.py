06ff71d5d99df4cc9a0d7ab92b72df73f982eef4
vipabh2 <a8919832@googlemail.com>
2025-03-23 03:33:39 +0300

    Update code.py

--- links/code/code/code.py
+++ links/code/code/code.py
@@ -918,25 +918,6 @@ async def calc(event):
             await event.respond("عملية غير مدعومة!", reply_to=event.message.id)
     except ZeroDivisionError:
         await event.respond("خطأ: لا يمكن القسمة على صفر!", reply_to=event.message.id)
-abh = [
-    "ها",
-    "تفظل",
-    "كول",
-    "اسمعك",
-    "شرايد",
-    "خلصني",
-    "https://t.me/VIPABH/1214",
-    "https://t.me/VIPABH/1215"
-]
-@ABH.on(events.NewMessage(pattern=r'^مخفي$'))
-async def reply(event):
-    if event.is_reply:
-        return
-    vipabh = random.choice(abh)
-    if vipabh.startswith("http"):
-        await event.reply(file=vipabh)
-    else:
-        await event.reply(vipabh)
 @ABH.on(events.NewMessage(pattern='ابن هاشم'))
 async def reply_abh(event):
     if event.chat_id == -1001968219024:
