433c5dd6835e6ab326ac0d4f8ba85454099d730f
vipabh2 <a8919832@googlemail.com>
2025-03-23 04:25:59 +0300

    Create main.py

---
+++ links/main.py
@@ -0,0 +1,11 @@
+# main.py
+from modules.module1 import function1
+from modules.module2 import function2
+
+def main():
+    print("Starting project...")
+    function1()
+    function2()
+
+if __name__ == "__main__":
+    main()
