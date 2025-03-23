85cbfb5d9554e4e78993d6bc417f42889e2b7965
vipabh2 <a8919832@googlemail.com>
2025-03-24 01:06:51 +0300

    Update __init__.py

--- links/code/__init__.py
+++ links/code/__init__.py
@@ -1,6 +1,6 @@
 import os
 from telethon import TelegramClient, events, Button
-
+from .. import *
 try:
     api_id = os.getenv('API_ID')
     api_hash = os.getenv('API_HASH')
@@ -15,3 +15,19 @@ try:
 
 except Exception as e:
     print(f"An error occurred: {e}")
+    from code.module1 import some_function
+from code.module2 import function2
+print("Running main.py...")
+some_function()
+another_function()  # تشغيل الوظيفة من module2.py
+def main():
+    print("This is the main program!")
+    some_function()
+    function2()
+if __name__ == "__main__":
+    main()
+
+# تشغيل البوت
+print("Starting Telegram bot...")
+ABH.run_until_disconnected()
+
