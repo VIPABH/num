a5a25b75fb9665894e049b2385c39c811f11bf02
vipabh2 <a8919832@googlemail.com>
2025-03-23 18:10:31 +0300

    Update main.py

--- links/main.py
+++ links/main.py
@@ -1,6 +1,6 @@
 # main.py
-from models import some_function  # استيراد دالة من ملف models.py
-from memes import another_function  # استيراد دالة من ملف memes.py
+from models import some_function
+from memes import another_function
 
 def main():
     print("This is the main program!")
