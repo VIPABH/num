49a2070b01bdffda02fe750be35e350c43b9249a
vipabh2 <a8919832@googlemail.com>
2025-03-24 01:05:09 +0300

    Update main.py

--- links/main.py
+++ links/main.py
@@ -6,19 +6,17 @@ another_function()  # تشغيل الوظيفة من module2.py
 
 # تشغيل البوت
 print("Starting Telegram bot...")
-ABH.run_until_disconnected()
-
-from telethon import TelegramClient
-import os
-api_id = os.getenv('API_ID')      
-api_hash = os.getenv('API_HASH')  
-bot_token = os.getenv('BOT_TOKEN')
-ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
-print('its run')
+# from telethon import TelegramClient
+# import os
+# api_id = os.getenv('API_ID')      
+# api_hash = os.getenv('API_HASH')  
+# bot_token = os.getenv('BOT_TOKEN')
+# ABH = TelegramClient('code', api_id, api_hash).start(bot_token=bot_token)
+# print('its run')
 def main():
     print("This is the main program!")
     some_function()
     function2()
 if __name__ == "__main__":
     main()
-ABH.run_until_disconnected()
+# ABH.run_until_disconnected()
