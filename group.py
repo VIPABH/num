4384a12ea448d083ab71518814f8ecf368b49195
VIPABH <a891983.2@gmail.com>
2025-09-19 01:47:38 +0300

    Update group.py

--- group.py
+++ group.py
@@ -12,6 +12,51 @@ from ABH import ABH, events
 from other import botuse
 from Resources import *
 from Program import chs
+from Resources import info
+from telethon.tl.types import (
+    MessageExtendedMediaPreview, DocumentAttributeAudio, DocumentAttributeSticker,
+    Message, MessageMediaPhoto, MessageMediaDocument, MessageMediaGeo,
+    DocumentAttributeVideo, DocumentAttributeAnimated,
+    MessageMediaPoll, MessageExtendedMedia,
+)
+def get_message_type(e):
+    m=e.message
+    if msg is None:
+        return
+    if msg.message and not msg.media:
+        return "الرسائل"
+    if isinstance(msg.media,MessageExtendedMediaPreview) or isinstance(msg.media,MessageExtendedMedia):
+        inner=msg.media.media
+        return get_message_type(Message(id=msg.id,media=inner))
+    if isinstance(msg.media,MessageMediaPhoto):
+        return "الصور"
+    if isinstance(msg.media,MessageMediaDocument):
+        for attr in msg.media.document.attributes:
+            if isinstance(attr,DocumentAttributeAnimated):
+                return "GIF"
+        for attr in msg.media.document.attributes:
+            if isinstance(attr,DocumentAttributeVideo):
+                if getattr(attr,"round_message",False):
+                    return "الفويس نوت"
+                return "الفيديو"
+        for attr in msg.media.document.attributes:
+            if isinstance(attr,DocumentAttributeSticker):
+                return "الستيكرات"
+            if isinstance(attr,DocumentAttributeAudio):
+                return "الفويسات" if getattr(attr,"voice",False) else "الصوتيات"
+        mime=msg.media.document.mime_type or ""
+        if mime.startswith("image/"):
+            return "الصور"
+        elif mime.startswith("video/"):
+            return "الفيديوهات"
+        elif mime.startswith("audio/"):
+            return "الصوتيات"
+        return "الملفات"
+    if isinstance(msg.media,MessageMediaGeo):
+        return "المواقع"
+    if isinstance(msg.media,MessageMediaPoll):
+        return "الاستفتاءات"
+    return
 @ABH.on(events.NewMessage(pattern='^سرقة|سرقه|خمط$'))
 async def theft(e):
     r = await e.get_reply_message()
