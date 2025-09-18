45715012860cd692034e5fd63a053e0af60dc342
VIPABH <a891983.2@gmail.com>
2025-09-19 02:17:23 +0300

    Update group.py

--- group.py
+++ group.py
@@ -22,32 +22,32 @@ from telethon.tl.types import (
     DocumentAttributeVideo, DocumentAttributeAnimated,
     MessageMediaPoll, MessageExtendedMedia,
 )
-def get_message_type(msg:Message)->str:
+
+def get_message_type(msg: Message) -> str:
     if msg is None:
-        print('none')
-        return
+        return "اخرى"
     if msg.message and not msg.media:
         return "الرسائل"
-    if isinstance(msg.media,MessageExtendedMediaPreview) or isinstance(msg.media,MessageExtendedMedia):
-        inner=msg.media.media
-        return get_message_type(Message(id=msg.id,media=inner))
-    if isinstance(msg.media,MessageMediaPhoto):
+    if isinstance(msg.media, MessageExtendedMediaPreview) or isinstance(msg.media, MessageExtendedMedia):
+        inner = msg.media.media
+        return get_message_type(Message(id=msg.id, media=inner))
+    if isinstance(msg.media, MessageMediaPhoto):
         return "الصور"
-    if isinstance(msg.media,MessageMediaDocument):
+    if isinstance(msg.media, MessageMediaDocument):
         for attr in msg.media.document.attributes:
-            if isinstance(attr,DocumentAttributeAnimated):
+            if isinstance(attr, DocumentAttributeAnimated):
                 return "الgif"
         for attr in msg.media.document.attributes:
-            if isinstance(attr,DocumentAttributeVideo):
-                if getattr(attr,"round_message",False):
+            if isinstance(attr, DocumentAttributeVideo):
+                if getattr(attr, "round_message", False):
                     return "الفويس نوت"
                 return "الفيديو"
         for attr in msg.media.document.attributes:
-            if isinstance(attr,DocumentAttributeSticker):
+            if isinstance(attr, DocumentAttributeSticker):
                 return "الستيكرات"
-            if isinstance(attr,DocumentAttributeAudio):
-                return "الفويسات" if getattr(attr,"voice",False) else "الصوتيات"
-        mime=msg.media.document.mime_type or ""
+            if isinstance(attr, DocumentAttributeAudio):
+                return "الفويسات" if getattr(attr, "voice", False) else "الصوتيات"
+        mime = msg.media.document.mime_type or ""
         if mime.startswith("image/"):
             return "الصور"
         elif mime.startswith("video/"):
@@ -55,11 +55,11 @@ def get_message_type(msg:Message)->str:
         elif mime.startswith("audio/"):
             return "الصوتيات"
         return "الملفات"
-    if isinstance(msg.media,MessageMediaGeo):
+    if isinstance(msg.media, MessageMediaGeo):
         return "المواقع"
-    if isinstance(msg.media,MessageMediaPoll):
+    if isinstance(msg.media, MessageMediaPoll):
         return "الاستفتاءات"
-    return
+    return "اخرئ"
 @ABH.on(events.NewMessage(pattern='معلومات'))
 async def track_messages(e):
     m=e.message
