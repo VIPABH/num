b1e9bd50c71eac3972522e491b967815c36afa62
VIPABH <a891983.2@gmail.com>
2025-09-19 12:28:39 +0300

    Update المستمع.py

--- المستمع.py
+++ المستمع.py
@@ -23,8 +23,7 @@ async def litsin_to_all(e):
   if text in ('معلوماتي', 'معلومات'):
       user_stats = await info(e, None)
       stats_str = "\n".join(
-          # f"{i+1} ⟶ {k} : {v}"
-          f"▢ ⟶ {k} : {v}"
+          f"{i+1} ◉ ↔ {k}: {v}"
           for i, (k, v) in enumerate(user_stats.items())
       )
       await e.reply(f"📊 إحصائياتك الحالية:\n\n{stats_str}")
