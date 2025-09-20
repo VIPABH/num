d8fe7dc56bb1fb1d01f59084ace62bbd3a6663b4
VIPABH <a891983.2@gmail.com>
2025-09-20 15:52:47 +0300

    Update Program.py

--- Program.py
+++ Program.py
@@ -166,7 +166,9 @@ async def remove_secondary_dev(event):
     chat_id = str(event.chat_id)
     user_id = str(entity.id)
     print(x)
-    if user_id in x[chat_id]:
+    print(user_id)
+    print(chat_id)
+    if chat_id in x and user_id in x[chat_id]:
         await chs(event, "عزيزي ابن هاشم هذا مو مطور ثانوي .")
         return
     dev = f"{event.chat_id}:{entity.id}"
