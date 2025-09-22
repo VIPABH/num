d6887dfec7eceff7f5789c5d765c57cd76a6b628
VIPABH <a891983.2@gmail.com>
2025-09-23 01:29:08 +0300

    Update other.py

--- other.py
+++ other.py
@@ -336,7 +336,7 @@ def ask_ai(q):
         return "صار خطأ بالسيرفر، جرب بعدين."
 @ABH.on(events.NewMessage(pattern=r"^مخفي\s*(.*)"))
 async def ai_handler(event):
-    user_q = event.pattern_match.group(1).strip()
+    user_q = event.pattern_match.group(1)
     x = event.text
     ignore_phrases = ["مخفي اعفطلة", "مخفي اعفطله", "مخفي قيده", "مخفي قيدة", "مخفي طكة زيج", "مخفي اطلع", "مخفي غادر", "مخفي نزلني"]
     if not user_q or x in ignore_phrases:
