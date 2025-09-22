ba337a711dfef2b58026e8f107dceb4414012c4f
VIPABH <a891983.2@gmail.com>
2025-09-22 17:11:28 +0300

    Update guard.py

--- guard.py
+++ guard.py
@@ -162,7 +162,7 @@ async def monitor_messages(event):
     now = int(time.time())
     all_data = res(None)
     if chat_id in all_data and user_id in all_data[chat_id]:
-        end_time = all_data[chat_id][user_id]
+        end_time = all_data[str(chat_id)][str(user_id)]
         try:
             remaining = end_time - now  
             rights = ChatBannedRights(
