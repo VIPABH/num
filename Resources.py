a90d6cac2e6d4127bc6e7bc19a23d58ac1bb5aa3
VIPABH <a891983.2@gmail.com>
2025-09-23 17:32:35 +0300

    Update Resources.py

--- Resources.py
+++ Resources.py
@@ -225,10 +225,9 @@ async def try_forward(event):
     gidvar = await LC(event.chat_id)
     if not gidvar:
         return False
-    r = await event.get_reply_message()
     await ABH.forward_messages(
         entity=int(gidvar),
-        messages=r.id,
+        messages=event.id,
         from_peer=event.chat_id
     )
     return True
