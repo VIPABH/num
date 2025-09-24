060e168c286bbbbaa55750ae7d1f7bbe1125871e
VIPABH <a891983.2@gmail.com>
2025-09-24 07:58:10 +0300

    Update count.py

--- count.py
+++ count.py
@@ -19,7 +19,6 @@ def save_data(data):
     with open(DATA_FILE, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 count = load_data()
-@ABH.on(events.NewMessage)
 async def unified_handler(event):
     if not event.is_group:
         return
