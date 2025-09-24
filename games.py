b136ab8c0b7aae618743eb73ed6f6c3a6148d675
VIPABH <a891983.2@gmail.com>
2025-09-24 14:49:35 +0300

    Update games.py

--- games.py
+++ games.py
@@ -1,12 +1,33 @@
-from Resources import football, questions, mention, ment, wfffp, react #type: ignore
 from top import points, add_points #type: ignore
 from datetime import datetime, timedelta
 import random, asyncio, time, os, json
 from telethon import Button, events
 from ABH import ABH #type: ignore
 from other import botuse
+from Resources import *
 from faker import Faker
-math_sessions={}
+x_arsessions = {}
+async def xargame(e):
+    if not e.is_group:
+        return
+    t = e.text
+    chat_id = e.chat_id
+    if t == "اعلام":
+        type = "اعلام"
+        await botuse(type)
+        if not chat_id in x_arsessions:
+            key, value = random.choice(list(x_ar.items()))
+            x_arsessions[chat_id] = key
+            await e.reply(f"ما اسم هذه الدولة؟\n\n{value}")
+    elif t == x_arsessions[chat_id]:
+        p = random.choice([1000, 5000])
+        user_id = e.sender_id
+        gid = e.chat_id
+        add_points(user_id, gid, points, amount=p)
+        await e.reply(f"اجابة صحيحة 🎉 \n ربحت {p} دينار.")
+        await react(e, '🎉')
+        del x_arsessions[chat_id]
+math_sessions = {}
 @ABH.on(events.NewMessage(pattern='^رياضيات|/math$'))
 async def math_handler(event):
  if not event.is_group:
