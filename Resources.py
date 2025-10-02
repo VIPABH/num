99f794f1baa19a5a702c58894f04807f9f29b134
VIPABH <a891983.2@gmail.com>
2025-10-02 19:09:00 +0300

    Update Resources.py

--- Resources.py
+++ Resources.py
@@ -458,8 +458,7 @@ async def hint(e):
 async def mention(event):
     name = getattr(event.sender, 'first_name', None) or 'غير معروف'
     user_id = event.sender_id
-    return f"[{name}](tg://user?id={user_id})"
-async def ment(entity):
+    return f"[{name}](tg://user?id={user_id})"async def ment(entity):
     try:
         if hasattr(entity, "id") and hasattr(entity, "first_name"):
             name = getattr(entity, "first_name", "غير معروف")
@@ -469,7 +468,7 @@ async def ment(entity):
             sender = entity.sender or await entity.get_sender()
             name = getattr(sender, "first_name", "غير معروف")
             user_id = sender.id
-            return f"[{name}](tg://user?id={
+            return f"[{name}](tg://user?id={user_id})"
         if hasattr(entity, "id"):
             name = getattr(entity, "first_name", "غير معروف")
             user_id = entity.id
