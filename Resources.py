a38f84be6d80a8a62ffb874c804ad161baf7efca
VIPABH <a891983.2@gmail.com>
2025-10-07 17:05:54 +0300

    Update Resources.py

--- Resources.py
+++ Resources.py
@@ -7,7 +7,7 @@ from telethon.tl.functions.messages import SendReactionRequest
 from telethon.tl.functions.messages import GetFullChatRequest
 from telethon.tl.types import ChatParticipantCreator
 from telethon.tl.types import ReactionEmoji
-import pytz, os, json, asyncio, time
+import pytz, os, json, asyncio, time, re
 import google.generativeai as genai
 from ABH import ABH
 async def is_owner(chat_id, user_id):
