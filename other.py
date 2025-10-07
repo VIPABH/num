66efee1b631c09e4d257b61022fe67d74f3ae14f
VIPABH <a891983.2@gmail.com>
2025-10-07 14:12:00 +0300

    Update other.py

--- other.py
+++ other.py
@@ -323,8 +323,15 @@ def ask_ai(q):
 async def ai_handler(event):
     user_q = event.pattern_match.group(1)
     x = event.text
-    ignore_phrases = ["مخفي اعفطلة", "مخفي اعفطله", "مخفي قيده", "مخفي قيدة", "مخفي طكة زيج", "مخفي اطلع", "مخفي غادر", "مخفي نزلني", "مخفي نزلة", "مخفي نزله", "مخفي اختار", "مخفي اختار"]
-    if not user_q or x in ignore_phrases:
+    ignore_phrases = [
+    "مخفي اعفطلة", "مخفي اعفطله",
+    "مخفي قيده", "مخفي قيدة",
+    "مخفي طكة زيج",
+    "مخفي اطلع", "مخفي غادر",
+    "مخفي نزلني", "مخفي نزلة", "مخفي نزله",
+    "مخفي اختار"
+]
+    if not user_q or x in ignore_phrases or x.startswith("مخفي اختار"):
         return
     type = "ai"
     await botuse(type)
