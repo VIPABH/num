7afd071ed60a75e3d83c87dc143bbd8267ee06cd
VIPABH <a891983.2@gmail.com>
2025-11-01 23:22:10 +0300

    Update guard.py

--- guard.py
+++ guard.py
@@ -475,7 +475,7 @@ async def warn_user(event):
         return
     chat_id = event.chat_id
     auth1 = await auth(event)
-    auth2 = await auth(event, False)
+    auth2 = await auth(event, True)
     if not auth1:
         await chs(event, 'شني خالي كبينه ')
         return
